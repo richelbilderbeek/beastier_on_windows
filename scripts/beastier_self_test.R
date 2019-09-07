6eb90bcec654df4ae98c72813d10b9e6e8651409
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-07 15:18:59 +0200

    Do self-test

---
+++ scripts/beastier_self_test.R
@@ -0,0 +1,53 @@
+# beastier self-testing script
+
+print("=====================")
+print("Self-testing beastier")
+print("=====================")
+
+library(beastier)
+
+print("------------")
+print("Session info")
+print("------------")
+
+print(sessionInfo())
+
+print("--------------------------")
+print("beastier's package version")
+print("--------------------------")
+
+print(packageVersion("beastier"))
+
+print("--------------------------")
+print("Show functions in package ")
+print("--------------------------")
+
+print(lsf.str("package:beastier"))
+
+print("---------------")
+print("Start self-test")
+print("---------------")
+
+library(testthat)
+
+testit::assert(is_beast2_installed())
+
+# Do a BEAST2 run
+beast2_options <- create_beast2_options(
+  input_filename = get_beastier_path("2_4.xml")
+)
+
+expect_false(file.exists(beast2_options$output_log_filename))
+expect_false(file.exists(beast2_options$output_trees_filenames))
+expect_false(file.exists(beast2_options$output_state_filename))
+
+output <- run_beast2_from_options(beast2_options)
+
+expect_true(length(output) > 40)
+expect_true(file.exists(beast2_options$output_log_filename))
+expect_true(file.exists(beast2_options$output_trees_filenames))
+expect_true(file.exists(beast2_options$output_state_filename))
+
+print("============================")
+print("Self-test of beastier passed")
+print("============================")
