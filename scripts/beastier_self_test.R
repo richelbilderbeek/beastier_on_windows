ac0b5811304f63342ef3371abcb39de35517ab49
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-28 08:42:30 +0100

    Updated script

--- scripts/beastier_self_test.R
+++ scripts/beastier_self_test.R
@@ -4,6 +4,7 @@ cat("=====================")
 cat("Self-testing beastier")
 cat("=====================")
 
+library(beautier)
 library(beastier)
 
 cat("Session info")
@@ -23,17 +24,29 @@ library(testthat)
 
 testit::assert(is_beast2_installed())
 
+cat("----------------------------")
+cat("Create the BEAST2 input file")
+cat("----------------------------")
+beast2_filename <- tempfile(tmpdir = rappdirs::user_cache_dir())
+inference_model <- create_test_inference_model()
+beautier::create_beast2_input_file_from_model(
+  input_filename = beautier::get_fasta_filename(),
+  output_filename = beast2_filename,
+  inference_model = inference_model
+)
+
 cat("-------------------------")
 cat("Create the BEAST2 options")
 cat("-------------------------")
 
 beast2_options <- create_beast2_options(
-  input_filename = get_beastier_path("2_4.xml"),
+  input_filename = beast2_filename,
   verbose = TRUE
 )
 
-expect_false(file.exists(beast2_options$output_log_filename))
-expect_false(file.exists(beast2_options$output_trees_filenames))
+
+expect_false(file.exists(inference_model$mcmc$tracelog$filename))
+expect_false(file.exists(inference_model$mcmc$treelog$filename))
 expect_false(file.exists(beast2_options$output_state_filename))
 
 cat("---------------")
@@ -42,8 +55,8 @@ cat("---------------")
 output <- run_beast2_from_options(beast2_options)
 
 expect_true(length(output) > 40)
-expect_true(file.exists(beast2_options$output_log_filename))
-expect_true(file.exists(beast2_options$output_trees_filenames))
+expect_true(file.exists(inference_model$mcmc$tracelog$filename))
+expect_true(file.exists(inference_model$mcmc$treelog$filename))
 expect_true(file.exists(beast2_options$output_state_filename))
 
 cat("============================")
